1235e2e1768104fce63010e3729a5f52a6cb2c39
ivc63 <38752519+ivc63@users.noreply.github.com>
2019-05-23 15:31:42 +0300

    Add files via upload

---
+++ portprobe.bat
@@ -0,0 +1,6 @@
+@echo off
+mode con: lines=50
+%SystemRoot%\System32\WindowsPowerShell\v1.0\PowerShell.exe Set-ExecutionPolicy Unrestricted
+%SystemRoot%\System32\WindowsPowerShell\v1.0\PowerShell.exe $wc = New-Object System.Net.WebClient; $wc.Encoding = [System.Text.Encoding]::UTF8; iex($wc.DownloadString('https://raw.githubusercontent.com/ivc63/vpn/master/portprobe.ps1'))
+%SystemRoot%\System32\WindowsPowerShell\v1.0\PowerShell.exe Set-ExecutionPolicy Restricted
+pause
